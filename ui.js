4afbed6749e835383613902c07f7660a9ce27652
Nicholas264 <nicholaspetrone12@gmail.com>
2018-02-18 16:52:45 -0800

    added camera feed

--- ui.js
+++ ui.js
@@ -195,7 +195,7 @@ NetworkTables.addKeyListener("/SmartDashboard/position", (key,val)=>{
 	var botx = arr[1];
 	var boty = arr[0];
 	var botangle = arr[2];
-	ui.position.style = "left: "+(botx+15)+"px;top: "+(boty+15)+"px;transform: rotate("+botangle+"deg);";
+	ui.position.style = "left: "+(botx+14)+"px;top: "+(boty+14)+"px;transform: rotate("+botangle+"deg);";
 })
 NetworkTables.addKeyListener("/SmartDashboard/destination", (key,val) =>{
 	var str = val.split(",");
@@ -206,7 +206,7 @@ NetworkTables.addKeyListener("/SmartDashboard/destination", (key,val) =>{
 	var botx = arr[1];
 	var boty = arr[0];
 	var botangle = arr[2];
-	ui.destination.style = "left: "+(botx+15)+"px;top: "+(boty+15)+"px;transform: rotate("+botangle+"deg);";
+	ui.destination.style = "background-color:Aqua; left: "+(botx+14)+"px;top: "+(boty+14)+"px;transform: rotate("+botangle+"deg);";
 })
 // Load list of prewritten autonomous modes
 NetworkTables.addKeyListener('/SmartDashboard/time_running', (key, value) => {
