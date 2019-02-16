c3e92a3749941997d95258570aa38b23c4a1dd49
Nick <nick-ls@users.noreply.github.com>
2019-02-16 13:19:52 -0800

    Remake image refresher
    It now no longer requires request. It checks to see if the image has an error, removes it if it does, and replaces it.

--- refreshImage.js
+++ refreshImage.js
@@ -1,20 +1,11 @@
 var reImage = document.getElementById("refresh");
 const feed = document.getElementById("cameraFeed");
-const request = require("request");
-
 var feedAddress = "http://10.47.38.2:1181/stream.mjpg";
 
-function pollImageServer() {
-	request(feedAddress,{encoding:"binary"},(err,res,data)=>{
-		if (!err&&res.statusCode==200) {
-			if (res.headers["content-length"]<500) {
-				resetImage(false);
-			}
-		} else {
-			resetImage(false);
-		}
-	});
+reImage.onerror = function() {
+	resetImage(false)
 }
+
 function resetImage(add) {
 	if (!add) {
 		feed.removeChild(reImage);
@@ -24,6 +15,8 @@ function resetImage(add) {
 	img.id = "refresh";
 	feed.appendChild(img);
 	reImage = document.getElementById("refresh");
+	reImage.onerror = function() {
+		resetImage(false);
+	}
 }
 resetImage(true);
-setInterval(pollImageServer,1500);
