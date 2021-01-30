eb16ce0970efea535be85870c8e34acdcc3391f7
Nicholas264 <22565232+Nicholas264@users.noreply.github.com>
2021-01-30 12:30:07 -0800

    Fix build structure of project

---
+++ src/app/components/Camera.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+interface CameraProps {
+
+}
+interface CameraState {
+
+}
+
+
+class Camera extends React.Component<CameraProps, CameraState> {
+
+	constructor(props: CameraProps) {
+		super(props);
+	}
+	public render() {
+		return <></>;
+	}
+}
