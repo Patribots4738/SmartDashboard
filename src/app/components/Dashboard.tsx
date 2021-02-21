12c2afc6e66492878858d5c3b506c9161f9d7f9d
Nicholas264 <22565232+Nicholas264@users.noreply.github.com>
2021-02-20 18:57:01 -0800

    Activate Cameras

--- src/app/components/Dashboard.tsx
+++ src/app/components/Dashboard.tsx
@@ -19,15 +19,15 @@ export default class Dashboard extends React.Component<DashProps, DashState> {
 	public render() {
 		return <>
 			<Column>
-				<Camera url="https://dummyimage.com/320x240/000/fff.png&text=Ball+Camera"/*"http://10.47.38.46:5800/stream.mjpg"*/></Camera>
+				<Camera url="http://10.47.38.46:5800/stream.mjpg" /*https://dummyimage.com/320x240/000/fff.png&text=Ball+Camera*/ ></Camera>
 				<RadioIndicator ntTableKey="driveMode" values={["Torque", "Speed"]}></RadioIndicator>
 				<Slider min={0.1} max={1} initial={0.6} step={0.05} ntTableKey="trainingSpeed" title="Speed Multiplier"></Slider>
 			</Column>
 			<Column>
-				<Camera url="https://dummyimage.com/240x160/000/fff.png&text=Driver+Camera"/*"http://10.47.38.109:1181/stream.mjpg"*/ maxheight={true}></Camera>
+				<Camera url="http://10.47.38.109:1181/stream.mjpg" /*https://dummyimage.com/240x160/000/fff.png&text=Driver+Camera*/ maxheight={true}></Camera>
 			</Column>
 			<Column>
-				<Camera url="https://dummyimage.com/320x240/000/fff.png&text=Shooter+Camera"/*"http://10.47.38.42:5800/stream.mjpg"*/></Camera>
+				<Camera url="http://10.47.38.42:5800/stream.mjpg" /*https://dummyimage.com/320x240/000/fff.png&text=Shooter+Camera*/></Camera>
 				<div className="indicatorBox">
 					<Indicator title="Angle from target" units={"\xB0"} ntTableKey="angleFromTarget"></Indicator>
 					<Indicator title="Is Aligned" units="" ntTableKey="aligned"></Indicator>
