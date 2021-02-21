48c5afc0a31f4cff1fef693763bee786cfdd9ee5
Nicholas264 <22565232+Nicholas264@users.noreply.github.com>
2021-02-20 20:33:12 -0800

    Add camera refresher

--- src/app/components/Camera.tsx
+++ src/app/components/Camera.tsx
@@ -1,22 +1,48 @@
 import * as React from "react";
 
+const fetch = require("node-fetch");
+
 interface CameraProps {
 	url: string;
 	maxheight?: boolean;
 }
 interface CameraState {
-
+	setUrl: string;
 }
 
 export default class Camera extends React.Component<CameraProps, CameraState> {
 	public static defaultProps = {maxheight: false} as CameraProps;
 	constructor(props: CameraProps) {
 		super(props);
+		this.state = {
+			setUrl: this.props.url
+		}
+		this.pollServer();
+	}
+	pollServer() {
+		fetch(this.props.url).then((res: any) => {
+			if (res) {
+				if (Number(res.headers.get("content-length")) < 100) {
+					this.reloadStream();
+				}
+			} else {
+				this.reloadStream();
+			}
+			setTimeout(this.pollServer, 500);
+		}).catch((e: unknown) => {
+			this.reloadStream();
+			setTimeout(this.pollServer, 500);
+		});
+		
+	}
+	reloadStream() {
+		this.setState({setUrl: "https://dummyimage.com/240x160/000/fff.png"});
+		this.setState({setUrl: this.props.url});;
 	}
 	public render() {
-		return <img className="camera" src={this.props.url} style={{
+		return <img className="camera" src={this.state.setUrl} style={{
 			width: this.props.maxheight ? "auto" : "100%",
 			height: this.props.maxheight ? "100%" : "auto"
-		}}></img>;
+		}} onError={this.reloadStream.bind(this)}></img>;
 	}
 }
